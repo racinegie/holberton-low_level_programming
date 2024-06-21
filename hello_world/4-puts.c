3c5145c1c3eb3c7a30db7b3d26a7bff772f03f2c
racinegie <racine.gielessen@gmaIL.com>
2024-06-21 01:54:46 -0700

    Write a C program that prints exactly Programming is like building a multilingual puzzle, followed by a new line.

---
+++ hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdio.h>
+
+/**
+ * main - entry point 
+ *
+ * Return: Always 0 (Success)
+ */
+int main (void)
+{
+	puts("/"Programming is like
+
+			return (0);
+	    
+			}
+
