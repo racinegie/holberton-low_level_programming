5c40945e255e2103f131ed2a255d2ed52bf46e2b
racinegie <racine.gielessen@gmaIL.com>
2024-06-21 13:53:31 +0000

    change

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -4,7 +4,7 @@
 */
 int main(void)
 {
-	printf("size of a char: %1 byte(s)\n", sizeof(char));
+	printf("size of a char: %1u byte(s)\n", sizeof(char));
 	printf("size of a int: %1u byte(s)\n,", sizeof (int));
 	printf("size of a long int: %1u byte(s)\n",sizeof (long int));
 	printf("size of a long long int:%1u byte(s)\n",sizeof(long long int));       
