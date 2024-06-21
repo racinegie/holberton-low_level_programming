b36c0faa8db535dd10b217f66c2313b601ec462e
racinegie <racine.gielessen@gmaIL.com>
2024-06-21 09:23:22 +0000

    update bettystyle

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -2,13 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - entry point 
+ * main - entry point
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);	
+	return (0);
 }
 
